a4714d5d0e0aa0e22db62f662d958c20943911e9
Kartik K Khullar <kartikkhullar840@gmail.com>
2020-08-27 04:36:35 +0530

    location of file as argument

--- Attendance100.py
+++ Attendance100.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import smtplib
 import time
+import sys
 import csv
 import os
 
@@ -21,12 +22,11 @@ def send_mail(day, curr_time, link):
     server.sendmail(sent_from, to, email_text)
     server.quit()
 
-def find_next_class(day):
+def find_next_class(timetable_location, day):
     current_time = datetime.now().strftime("%H:%M")
     curr_time = time.strptime(current_time, "%H:%M")
     
-    #Replace with COMPLETE path to attendance file below.
-    with open(r'E:\Programming\Python\Attendance100\timetable.csv', 'r') as timetable:
+    with open(timetable_location, 'r') as timetable:
         reader = csv.reader(timetable)
         for row in reader:
             class_time = time.strptime(str(row[0]), "%H:%M")
@@ -41,23 +41,27 @@ def find_next_class(day):
     print("Classes already over.")
     exit()
 
-Weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-day = datetime.today().weekday()
-print(Weekdays[day])
+def attend_class(timetable_location):
+    Weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    day = datetime.today().weekday()
+    print(Weekdays[day])
 
-while True:
-    class_time, link = find_next_class(day) 
     while True:
-        current_time = datetime.now().strftime("%H:%M")
-        curr_time = time.strptime(current_time, "%H:%M")
-        print("Current Time : " + current_time)
+        class_time, link = find_next_class(timetable_location, day) 
+        while True:
+            current_time = datetime.now().strftime("%H:%M")
+            curr_time = time.strptime(current_time, "%H:%M")
+            print("Current Time : " + current_time)
 
-        if class_time == curr_time:
-            print("Class at " + current_time)
-            os.system("start "" " + link)
-            print("Joined class at link : " + link)
+            if class_time == curr_time:
+                print("Class at " + current_time)
+                os.system("start "" " + link)
+                print("Joined class at link : " + link)
+                time.sleep(60)
+                #send_mail(Weekdays[day], current_time, link)
+                break
+
             time.sleep(60)
-            #send_mail(Weekdays[day], current_time, link)
-            break
 
-        time.sleep(60)
+if __name__ == "__main__":
+    attend_class(sys.argv[1])
