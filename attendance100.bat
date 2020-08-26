221373609ac084edaa15ff019eb3618d4f472b16
Kartik K Khullar <kartikkhullar840@gmail.com>
2020-08-27 04:47:05 +0530

    batch file format

--- attendance100.bat
+++ attendance100.bat
@@ -1,2 +1,2 @@
 echo off
-"<Path to python.exe>\python.exe" "<Path to python script>\Attendance100\Attendance100.py" "<Path to timetable file>\Attendance100\timetable.csv"
+"<Path to python.exe>\python.exe" "<Path to python script>\Attendance100.py" "<Path to timetable file>\timetable.csv"
